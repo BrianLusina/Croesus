ef3d36286e8cb01b275b3e9b61dda472899b5872
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 16:11:10 +0300

    Add representation field to model

--- teamdirectory/models.py
+++ teamdirectory/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, unique=True)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
 
     def __repr__(self):
-        return "<Name: % name>" % (self.name)
+        return "<id: %r, Name: %r, Email: %r, Title: %r>" % (self.id, self.name, self.email, self.title)
+
