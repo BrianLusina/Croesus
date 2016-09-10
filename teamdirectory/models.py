c9dd8310b62dabd139d6a3cdbe6fa861ca634b55
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 16:49:06 +0300

    Add team template

--- teamdirectory/models.py
+++ teamdirectory/models.py
@@ -4,11 +4,12 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True, unique=True)
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
 
     def __repr__(self):
-        return "<id: %r, Name: %r, Email: %r, Title: %r>" % (self.id, self.name, self.email, self.title)
-
+        return "<id: %r, Name:< %r %r>, Email: %r, Title: %r>" % (
+            self.id, self.first_name, self.last_name, self.email, self.title)
--- teamdirectory/views.py
+++ teamdirectory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Person
+
 
 # Create your views here.
+def team_directory(request):
+    context = {"person": Person.objects.all()}
+    return render(request=request, template_name="team.html", context=context)
+
