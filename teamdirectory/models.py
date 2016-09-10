3f4fa3608046aa979e3c5b68488f0cda264bcff0
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 17:10:14 +0300

    add migrations

--- teamdirectory/models.py
+++ teamdirectory/models.py
@@ -8,8 +8,11 @@ class Person(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     title = models.CharField(max_length=100)
+    linkedin_url = models.CharField(max_length=200, null=True)
+    github_url = models.CharField(max_length=200, null=True)
+    twitter_url = models.CharField(max_length=200, null=True)
     image = models.CharField(max_length=500)
 
     def __repr__(self):
-        return "<id: %r, Name:< %r %r>, Email: %r, Title: %r>" % (
-            self.id, self.first_name, self.last_name, self.email, self.title)
+        return "<id: %r, Title: %r, Name:<%r %r>, Email: %r ImgUrl: %r>" % (
+            self.id, self.title, self.first_name, self.last_name, self.email, self.image)
