9e55dd4df20933f1160f164287b0fcab0eb86171
Brian Lusina <lusinabrian@gmail.com>
2016-09-12 23:39:25 +0300

    Add member-edit template

--- teamdirectory/urls.py
+++ teamdirectory/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(regex=r'^$', view=views.team_directory),
+    url(regex=r'$', view=views.team_directory),
     url(regex=r'members/([a-z0-9-]+)/$', view=views.member_detail, name="member_detail"),
     url(regex=r'members/([a-z0-9-]+)/edit/$', view=views.member_edit, name='edit'),
 ]
--- teamdirectory/views.py
+++ teamdirectory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Person
-
+from .forms import PersonForm
 
 # Create your views here.
 def team_directory(request):
@@ -15,5 +15,7 @@ def member_detail(request, slug):
     return render(request=request, template_name="member_detail.html", context=context)
 
 
-def member_edit(request):
-    pass
+def member_edit(request, slug):
+    form = PersonForm
+    context = {"form":form}
+    return render(request=request, template_name='member_edit.html', context=context)
