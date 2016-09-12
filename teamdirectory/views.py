c35ea6d6131e63db4ba48fafdd555052b7fbf110
Brian Lusina <lusinabrian@gmail.com>
2016-09-12 23:52:24 +0300

    Map Persons to dictionary

--- teamdirectory/views.py
+++ teamdirectory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Person
 from .forms import PersonForm
-
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def team_directory(request):
@@ -17,6 +17,11 @@ def member_detail(request, slug):
 
 
 def member_edit(request, slug):
-    form = PersonForm
-    context = {"form": form}
-    return render(request=request, template_name='member_edit.html', context=context)
+    person = Person.objects.get(slug=slug)
+    if request.method == "POST":
+        pass
+    else:
+        person_dict = model_to_dict(person)
+        form = PersonForm(person_dict)
+        context = {"form": form}
+        return render(request=request, template_name='member_edit.html', context=context)
