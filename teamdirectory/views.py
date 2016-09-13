b342ba8a8c379532a28e038398333f5cdfb32f8b
Brian Lusina <lusinabrian@gmail.com>
2016-09-13 06:58:38 +0300

    add login/google/oath2 redirect

--- teamdirectory/views.py
+++ teamdirectory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Person
 from .forms import PersonForm
 from django.forms.models import model_to_dict
@@ -8,7 +8,10 @@ from django.core.urlresolvers import reverse
 # Create your views here.
 def team_directory(request):
     context = {"person": Person.objects.all()}
-    return render(request=request, template_name="team.html", context=context)
+    if request.user.is_authenticated():
+        return render(request, 'team.html', context=context)
+    else:
+        return redirect('/login/google-oauth2')
 
 
 # takes in a request and a slug fiel which will enable a display like this brian-lusina-ombito
@@ -31,3 +34,10 @@ def member_edit(request, slug):
         form = PersonForm(person_dict)
         context = {"form": form}
         return render(request=request, template_name='member_edit.html', context=context)
+
+
+def verify_email(backend, user, response, *args, **kwargs):
+    if backend.name == 'google-oauth2':
+        existing_person = Person.objects.filter(email=kwargs.get('details').get('email'))
+        if not existing_person:
+            return HttpResponse("You don't have access!")
