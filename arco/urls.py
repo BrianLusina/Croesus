9a0652b68520479840263a8ab911f3f744d0e89f
BrianLusina <lusinabrian@gmail.com>
2016-10-02 21:49:59 +0300

    Update www urls

--- arco/urls.py
+++ arco/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^team/', include('teamdirectory.urls')),
+    url(r'^/', include('www.urls')),
     url(r'', include('social.apps.django_app.urls', namespace=None)),
 ]
