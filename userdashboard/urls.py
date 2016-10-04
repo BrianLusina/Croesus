ecf2920d5e245a4c7acdde4702a447f17154f4e1
BrianLusina <lusinabrian@gmail.com>
2016-10-04 18:24:17 +0300

    Update static files

--- userdashboard/urls.py
+++ userdashboard/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(regex=r'$', view=views.dashboard, name="home"),
+    url(regex=r'$', view=views.dashboard, name="dashboard"),
 ]
