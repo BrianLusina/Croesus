642a417c0d52d2b875fda51f4d8392bc8231f600
BrianLusina <lusinabrian@gmail.com>
2016-10-04 19:04:04 +0300

    Update ui

--- userdashboard/urls.py
+++ userdashboard/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(regex=r'$', view=views.dashboard, name="dashboard"),
+    url(regex=r'user/', view=views.dashboard, name="dashboard"),
 ]
