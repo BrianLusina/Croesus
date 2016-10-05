60af69da1c7d24ebb916bc5b53bb810ff3ea6714
BrianLusina <lusinabrian@gmail.com>
2016-10-05 11:39:48 +0300

    Update urls for apps

--- userdashboard/views.py
+++ userdashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    render(request=request, template_name='userdashboard.html')
+    return render(request=request, template_name='userdashboard.html')
