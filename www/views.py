c4919da97c2764b2826b74e4c76adf20dd080f3b
BrianLusina <lusinabrian@gmail.com>
2016-10-05 12:43:04 +0300

    Update menu items, add horizontal timeline template

--- www/views.py
+++ www/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def home(request):
+    """
+    Entry point into the app
+    :param request that will be handle by the url
+    :return: renders the home page
+    """
     return render(request=request, template_name='index.html')
