d1eba50f70552ffac2cb0839e9ecd2c9e4642a64
BrianLusina <lusinabrian@gmail.com>
2016-10-08 13:25:14 +0300

    fixed overflow of about us page

--- www/urls.py
+++ www/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(regex=r'', view=views.home, name="home"),
+    url(regex=r'/about', view=views.home, name="about"),
+    url(regex=r'/contact', view=views.home, name="contact"),
 ]
