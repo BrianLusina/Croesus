7997860d5d9ce3d03ee10b1a5c2b4017686a4cf2
BrianLusina <lusinabrian@gmail.com>
2016-10-09 10:12:34 +0300

    Edit blog post container styles

--- www/views.py
+++ www/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from newspaper import Article
 
 
 def home(request):
@@ -8,3 +9,11 @@ def home(request):
     :return: renders the home page
     """
     return render(request=request, template_name='index.html')
+
+
+def fetch_news():
+    """
+    fetch the news and blog posts for the blogs and news section
+    :return: a dictionary with the blogs and news items
+    """
+    pass
