9c7287956ab9fdae2f800eabeb7e38952f4c349d
Brian Lusina <lusinabrian@gmail.com>
2017-05-01 17:15:04 +0300

    add __meta__.py, setup.py and CI yaml files

---
+++ server/app/__meta__.py
@@ -0,0 +1,10 @@
+"""
+Meta information for this project
+"""
+
+__project__ = 'Arco'
+__version__ = '0.1.1'
+__author__ = 'Brian Lusina'
+__email__ = 'lusinabrian@gmail.com'
+__url__ = 'http://arco.com'
+__copyright__ = 'Brian Lusina'
---
+++ server/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# diamdond-sample (c) Brian Lusina
+
+import re
+import os
+import codecs
+from setuptools import setup, find_packages
+from distutils.dir_util import copy_tree
+
+
+def read(*rnames):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *rnames), 'r', 'utf-8').read()
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, read('app/__meta__.py'))
+    return strval
+
+
+setup(
+    version=grep('__version__'),
+    name='Arco',
+    description="A flask Financial application",
+    packages=find_packages(),
+    scripts=[
+        "runserver.py",
+        "manage.py",
+    ],
+    long_description=read('README.md'),
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    include_package_data=True,
+    keywords='',
+    author=grep('__author__'),
+    author_email=grep('__email__'),
+    url=grep('__url__'),
+    install_requires=read('requirements.txt'),
+    license='MIT',
+    zip_safe=False,
+)
