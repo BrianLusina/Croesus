e519f7ae44d1f5d8585338a39603903d45714589
Brian Lusina <lusinabrian@gmail.com>
2017-05-06 17:04:38 +0300

    Action types
    action types define the actions to be passed to the store when the application is changing state. these actions will be dispatched to
    the store and the state will be changed accordingly

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,9 @@
+/**
+ * @author lusinabrian on 06/05/17.
+ * @Notes:
+ * Action creators are functions that return plain Javascript object of action type and an
+ * optional payload. So action creators create actions that are dispatched to the store.
+ * They are just pure functions. defines our action types in a file and export them for ease
+ * of use in other files. They’re constants and it’s a good practice to define them in a
+ * separate file(s).
+ */
