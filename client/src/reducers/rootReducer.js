83b628b887c50628231b8ba61fe920d097523a5c
Brian Lusina <lusinabrian@gmail.com>
2017-05-06 16:50:19 +0300

    RootReducer
    this registers all the reducers before passing them to the store. This will combine all the reducers into 1 single reducer function

---
+++ client/src/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+/**
+ * @author lusinabrian on 06/05/17.
+ * @Notes: Combines all reducers to a single reducer function
+ * This registers the reducers before passing it to the store
+ */
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
