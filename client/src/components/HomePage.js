bc2b0ff1d3a6e275009f32d9d1c5aea324bce65f
Brian Lusina <lusinabrian@gmail.com>
2017-05-20 20:48:29 +0300

    add variables and mixins

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,6 +4,15 @@
 import React, { Component } from 'react';
 
 export default class HomePage extends Component{
+
+    constructor(){
+        super();
+
+        this.handleOnLoginClick = this.handleOnLoginClick.bind(this);
+        this.handleOnSignUpClicked = this.handleOnSignUpClicked.bind(this);
+        this.handleOnAboutClicked = this.handleOnAboutClicked.bind(this);
+    }
+
     render(){
         return(
             <div className="page" id="page-home">
@@ -16,31 +25,50 @@ export default class HomePage extends Component{
                     <h1 className="bp-header__title">Financial Market Tracker</h1>
                     <p className="bp-header__desc">Sifts through the market, so you don't have to.</p>
                     <nav className="bp-nav">
-                        <a className="bp-nav__item bp-icon bp-icon--prev" href="" data-info="Login">
+                        <a className="bp-nav__item bp-icon bp-icon--prev"
+                           onClick={this.handleOnLoginClick} data-info="Login">
                             <span>Login</span>
                         </a>
 
-                        <a className="bp-nav__item bp-icon bp-icon--drop" href="" data-info="Sign Up">
+                        <a className="bp-nav__item bp-icon bp-icon--drop"
+                           onClick={this.handleOnSignUpClicked} data-info="Sign Up">
                             <span>Sign Up</span>
                         </a>
 
-                        <a className="bp-nav__item bp-icon bp-icon--archive" href="" data-info="About">
+                        <a className="bp-nav__item bp-icon bp-icon--archive"
+                           onClick={this.handleOnAboutClicked} data-info="About">
                             <span>About</span>
                         </a>
                     </nav>
                 </header>
 
                 <img className="poster" src="" alt="img01" />
-
-		</div>
+            </div>
         )
     }
 
     /**
-     * Open login modal
-     * @param {Object} e
+     * Handle login click event
+     * @param {Object} e event object
      * */
     handleOnLoginClick(e){
+        e.preventDefault();
+    }
+
+    /**
+     * Handle sign up click event
+     * @param {Object} e event object*/
+    handleOnSignUpClicked(e){
+        e.preventDefault();
+
+    }
+
+    /**
+     * Handle About click event
+     * @param {object} e event object
+     * */
+    handleOnAboutClicked(e){
+        e.preventDefault();
 
     }
 }
