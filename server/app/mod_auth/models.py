1f4a510a8f121c0479a6e60fc3dda3313ebbfeba
Brian Lusina <lusinabrian@gmail.com>
2017-05-21 20:41:46 +0300

    Add login routes
    Facebook login route will deal with facebook login
    the others do the same, as the names suggest

--- server/app/mod_auth/models.py
+++ server/app/mod_auth/models.py
@@ -1,17 +1,21 @@
 """
 Models dealing with authentication
+
+UserAccountStatus deals with the current account status details
+UserAccount deals with authenticating the user, will handle login and authentication details
+UserProfile is the actual user profile and will be used to display the user profile data
+ExternalServiceAccount is for storing data for external service providers
 """
-from sqlalchemy import Column, String, Integer, DateTime, func, ForeignKey, Boolean, Table
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Boolean
 from app.models import Base
 from abc import ABCMeta
-from hashlib import md5
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.ext.declarative import declared_attr
 from flask_login import UserMixin
 from .. import db, login_manager
 from datetime import datetime
-from sqlalchemy.orm import relationship, backref, dynamic
+from sqlalchemy.orm import relationship
 import json
 
 
@@ -40,11 +44,10 @@ class UserAccountStatus(db.Model):
         return "Id: {}, Code:{}, Name:{}".format(self.id, self.code, self.name)
 
 
-class UserAccount(Base):
+class UserAccount(Base, UserMixin):
     """
     User account model
     :cvar uuid unique user id
-    :cvar first_name first name of user
     :cvar username unique user name of user
     :cvar email email of user
     :cvar email_confirmation_token token for confirming this user 
@@ -94,17 +97,28 @@ class UserAccount(Base):
 
     def __repr__(self):
         return "Id: {},\n uuid: {}, Username: {} ProfileId:{}, AccountStatusId:{}" \
-               "\n Dates: [created: {}, modified: {}]\n " \
+               "Email:{} \n Dates: [created: {}, modified: {}]\n " \
                "Registered:{}\n Confirmed: [{} date: {}]\n Last Seen: {}" \
             .format(self.id, self.uuid, self.username, self.user_profile_id,
-                    self.user_account_status_id, self.date_created, self.date_modified,
-                    self.registered_on, self.confirmed, self.confirmed_on, self.last_seen)
+                    self.user_account_status_id, self.email, self.date_created,
+                    self.date_modified,self.registered_on, self.confirmed,
+                    self.confirmed_on, self.last_seen)
 
     def to_json(self):
-        pass
+        return dict(
+            id=self.id, uuid=self.uuid, username=self.username,
+            profile_id=self.user_profile_id, account_status_id=self.user_account_status_id,
+            email=self.email, date_created=self.date_created,
+            date_modified=self.date_modified, registerd_on=self.registered_on,
+            confimed=self.confirmed, confirmed_on=self.confirmed_on,
+            last_seen=self.last_seen
+        )
 
     def from_json(self, user_account):
-        pass
+        user = json.loads(user_account)
+        self.username = user["username"]
+        self.email = user["email"]
+        self.password_hash = user["password"]
 
 
 class UserProfile(Base):
@@ -254,13 +268,13 @@ class AsyncOperation(Base):
     user_profile_id = Column(Integer, ForeignKey(UserProfile.id))
 
     status = relationship("AsyncOperationStatus", foreign_keys=async_operation_status_id)
-    user_profile = relationship("UserAccount", foreign_keys=user_profile_id)
+    user_profile = relationship("UserProfile", foreign_keys=user_profile_id)
 
     def __repr__(self):
         pass
 
-        # todo add events
-        # event.listen(
-        #     AsyncOperationStatus.__table__, "after_create",
-        #     DDL(""" INSERT INTO async_operation_status (id,code) VALUES(1,'pending'),(2, 'ok'),(3, 'error'); """)
-        # )
+# todo add events
+# event.listen(
+#     AsyncOperationStatus.__table__, "after_create",
+#     DDL(""" INSERT INTO async_operation_status (id,code) VALUES(1,'pending'),(2, 'ok'),(3, 'error'); """)
+# )
--- server/app/mod_auth/views.py
+++ server/app/mod_auth/views.py
@@ -15,3 +15,27 @@ def signup():
 @auth.route("reset", methods=["GET", "POST"])
 def reset_password():
     pass
+
+
+@auth.route("facebook", methods=["GET", "POST"])
+def login_with_facebook():
+    """
+    Login user with facebook
+    """
+    pass
+
+
+@auth.route("google", methods=["GET", "POST"])
+def login_with_google():
+    """
+    Login user with facebook
+    """
+    pass
+
+
+@auth.route("twitter", methods=["GET", "POST"])
+def login_with_twitter():
+    """
+    Login user with facebook
+    """
+    pass
