a8d42f1e73d4d63dc50d83d6020ddc8fd02a5395
Brian Lusina <lusinabrian@gmail.com>
2017-05-21 19:22:03 +0300

    create auth models

---
+++ server/app/mod_auth/models.py
@@ -0,0 +1,72 @@
+"""
+Models dealing with authentication
+"""
+from sqlalchemy import Column, String, Integer, DateTime, func, ForeignKey, Boolean, Table
+from app.models import Base
+from hashlib import md5
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.ext.declarative import declared_attr
+from flask_login import UserMixin
+from .. import db, login_manager
+from datetime import datetime
+from sqlalchemy.orm import relationship, backref, dynamic
+
+
+class UserAccount(Base):
+    __tablename__ = "user_account"
+
+    def __repr__(self):
+        pass
+
+    def to_json(self):
+        pass
+
+    def from_json(self):
+        pass
+
+
+class UserProfile(Base):
+    __tablename__ = "user_profile"
+
+    def from_json(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def to_json(self):
+        pass
+
+
+# This callback is used to reload the user object from the user ID stored in the session
+@login_manager.user_loader
+def load_author(user_id):
+    return UserAccount.query.get(int(user_id))
+
+# class Person(models.Model):
+#     id = models.IntegerField(primary_key=True, auto_created=True, unique=True)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     slug = models.SlugField(max_length=1000, blank=True)
+#     email = models.CharField(max_length=200)
+#     title = models.CharField(max_length=100)
+#     linkedin = models.CharField(max_length=200, null=True)
+#     github = models.CharField(max_length=200, null=True)
+#     twitter = models.CharField(max_length=200, null=True)
+#     image = models.CharField(max_length=500)
+#     responsibilities = models.CharField(max_length=1000, default='')
+#     bio = models.CharField(max_length=1000, default='')
+#     birthday = models.DateField(default=timezone.now)
+#
+#     def save(self, *args, **kwargs):
+#         if not self.pk:
+#             self.slug = slugify(self.first_name + self.last_name)
+#         super(Person, self).save(*args, **kwargs)
+#
+#     def __repr__(self):
+#         return "<id: %r, Title: %r, Name:<%r %r>, Email: %r ImgUrl: %r>" % (
+#             self.id, self.title, self.first_name, self.last_name, self.email, self.image)
+#
+#     def __str__(self):
+#         return self.first_name
--- server/app/mod_auth/views.py
+++ server/app/mod_auth/views.py
@@ -2,6 +2,16 @@ from . import auth
 from flask import jsonify
 
 
-@auth.route("", methods=["GET", "POST"])
+@auth.route("login", methods=["GET", "POST"])
 def login():
+    pass
+
+
+@auth.route("signup", methods=["GET", "POST"])
+def signup():
+    pass
+
+
+@auth.route("reset", methods=["GET", "POST"])
+def reset_password():
     pass
